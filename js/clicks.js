f646a5491724145b5012d9ad46ee1addd5f3cb87
Casey Schneebeck <cschneebeck@tnc.org>
2017-06-13 12:45:05 -0600

    import code to get started

---
+++ js/clicks.js
@@ -0,0 +1,150 @@
+define([
+	"dojo/_base/declare", "esri/tasks/query", "esri/tasks/QueryTask"
+],
+function ( declare, Query, QueryTask ) {
+        "use strict";
+
+        return declare(null, {
+			eventListeners: function(t){
+				$("#" + t.id + "selectCountry").chosen({allow_single_deselect:true, width:"252px"})
+					.change(function(c){
+						var val = c.target.value;
+						// check for a deselect
+						if (val.length == 0){
+							$("#" + t.id + " .c-sel").slideUp();
+							t.obj.visibleLayers = [t.countries];
+							t.obj.visibleLayers1 = [-1];
+						}else{
+							t.layerDefs[t.selectedCountry] = "OBJECTID = " + val;
+							t.layerDefs1[t.selectedCountryFill] = "OBJECTID = " + val;
+							t.dynamicLayer.setLayerDefinitions(t.layerDefs);
+							t.dynamicLayer1.setLayerDefinitions(t.layerDefs1);
+							t.obj.visibleLayers = [t.selectedCountry, t.countries];
+							t.obj.visibleLayers1 = [t.selectedCountryFill];
+							$.each(t.atts,function(i,v){
+								if(val == v.OBJECTID){
+									var w = v.emiss_redux_1;
+									if (w != -99){
+										w = w * 100;
+										w = t.clicks.roundTo(w,0);
+										$("#" + t.id + "emiss_redux_1").html(w + "%" + v.eu)
+									}
+									else{
+										$("#" + t.id + "emiss_redux_1").html("N/A")
+									}
+									var w1 = v.ref_yr_1;
+									if (w1 == -99){
+										w1 = "N/A"
+									}
+									$("#" + t.id + "ref_yr_1").html(w1)
+									var x = v.emiss_cut
+									if (x != -99){
+										x = t.clicks.roundTo(x,2);
+										x = t.clicks.commaSeparateNumber(x);
+									}else{
+										x = "N/A"
+									}
+									$("#" + t.id + "emiss_cut").html(x)
+									var y = 0;
+									t.highVals = [];
+									t.maxVals = [];
+									t.lblArray = [];
+									$("#" + t.id + " .p-a").hide();
+									$.each(t.highArray,function(i1,v1){
+										if(v[v1] != -99){
+											t.highVals.push(v[v1]);
+											y = y + Number(v[v1]);
+										}else{
+											t.highVals.push(0);
+										}
+									})
+									$.each(t.maxArray,function(i1,v1){
+										t.lblArray.push(v[v1]);
+										if(v[v1] != -99){
+											if (v[v1] > 40){
+												$("#" + t.id + " .p-a:eq(" + i1 +")").show();
+											}
+											t.maxVals.push(v[v1]);
+										}else{
+											t.maxVals.push(0);
+										}
+									})
+									t.twoDeg = 0;
+									$.each(t.degArray,function(i1,v1){
+										if(v[v1] != -99){
+											t.twoDeg = t.twoDeg + v[v1];
+										}
+									})	
+									if (v.emiss_cut == -99){
+										t.parisBar = 0;	
+									}else{
+										t.parisBar = v.emiss_cut;
+									}
+									t.chartjs.updateChart(t);
+									y = t.clicks.roundTo(y, 4)
+									y = t.clicks.commaSeparateNumber(y)
+									$("#" + t.id + "nscMitPoten").html(y);
+								}
+							})
+							$("#" + t.id + " .c-sel").slideDown();
+							if (t.querySource == "menu"){
+								var c = $("#" + t.id + "selectCountry option:selected").text();
+								var q = new Query();
+								var qt = new QueryTask(t.url + "/" + t.countriesPoint);
+								q.where = "country ='" + c + "'";
+								q.returnGeometry = true;
+								qt.execute(q, function(e){
+									t.map.centerAndZoom(e.features[0].geometry,4)
+								});	
+							}	
+							t.querySource = "menu";
+						}
+						t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers)
+						t.dynamicLayer1.setVisibleLayers(t.obj.visibleLayers1)
+					});
+			},
+
+			makeVariables: function(t){
+				t.querySource = "menu";
+				t.selectedCountry = 0;
+				t.selectedCountryFill = 1;
+				t.countries = 2;
+				t.countriesAfterFirstSel = 3;
+				t.countriesPoint = 4;
+				t.layerDefs = [];
+				t.layerDefs1 = [];
+				t.atts = [];
+				t.highArray = ["defor_high", "wfuel_high", "mangrove_high","peat_loss_high","legumes_high","optint_high","rice_high","natfor_high","peat_res_high","refor_high"];
+				t.maxArray = ["refor_max", "defor_max", "natfor_max", "peat_res_max", "peat_loss_max", "wfuel_max", "mangrove_max", "rice_max", "optint_max", "legumes_max"];
+				t.degArray = ["mangrove_2deg","peat_loss_2deg","legumes_2deg","optint_2deg","rice_2deg","natfor_2deg","peat_res_2deg","refor_2deg"];
+			},
+			commaSeparateNumber: function(val){
+				while (/(\d+)(\d{3})/.test(val.toString())){
+					val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+				}
+				return val;
+			},
+			roundTo: function(n, digits) {
+				if (digits === undefined) {
+			    	digits = 0;
+			    }
+			    var multiplicator = Math.pow(10, digits);
+				n = parseFloat((n * multiplicator).toFixed(11));
+				var test =(Math.round(n) / multiplicator);
+				return +(test.toFixed(2));
+			},
+			abbreviateNumber: function(num) {
+			    if (num >= 1000000000) {
+			        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+			     }
+			     if (num >= 1000000) {
+			        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+			     }
+			     if (num >= 1000) {
+			        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+			     }
+			     return num;
+			}
+        });
+    }
+);
---
+++ js/esriapi.js
@@ -0,0 +1,77 @@
+define([
+	"esri/layers/ArcGISDynamicMapServiceLayer", "esri/geometry/Extent", "esri/SpatialReference", "esri/tasks/query" ,"esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", 
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color"
+],
+function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, QueryTask, declare, FeatureLayer, 
+			SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color ) {
+        "use strict";
+
+        return declare(null, {
+			esriApiFunctions: function(t){	
+				// Add dynamic map service
+				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url);
+				t.dynamicLayer1 = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.5});
+				t.map.addLayer(t.dynamicLayer1);
+				t.map.addLayer(t.dynamicLayer);
+				if (t.obj.visibleLayers.length > 0){	
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				}
+				t.dynamicLayer.on("load", function () { 			
+					t.layersArray = t.dynamicLayer.layerInfos;
+					if (t.obj.stateSet == "no"){
+						t.map.setZoom(2);
+					}
+					// Save and Share Handler					
+					if (t.obj.stateSet == "yes"){
+						//extent
+						var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
+						t.map.setExtent(extent, true);
+						t.obj.stateSet = "no";
+					}	
+				});		
+				// get data table
+				var q = new Query();
+				var qt = new QueryTask(t.url + "/" + t.countries);
+				q.where = "OBJECTID > 0";
+				q.returnGeometry = false;
+				q.outFields = ["*"];
+				var c = [];
+				qt.execute(q, function(e){
+					$.each(e.features, function(i,v){
+						t.atts.push(v.attributes)
+						c.push(v.attributes.country + "," +v.attributes.OBJECTID)
+					})
+					var countries = c.sort();
+					$.each(countries,function(i,v){
+						var a = v.split(",")[1];
+						var b = v.split(",")[0];
+						$('#' + t.id + 'selectCountry').append("<option value='" + a + "'>"+ b +"</option")
+					})	
+					$('#' + t.id + 'selectCountry').trigger("chosen:updated");		
+				});
+				// handle map clicks
+				t.map.setMapCursor("pointer")
+				t.map.on('click',function(c){
+					if (t.open == "yes"){
+						t.querySource = "map";
+						var pnt = c.mapPoint;
+						var q1 = new Query();
+						var qt1 = new QueryTask(t.url + "/" + t.countries);
+						q1.geometry = pnt;
+						q1.outFields = ["OBJECTID"];
+						qt1.execute(q1, function(e){
+							if (e.features.length > 0){
+								var obid = e.features[0].attributes.OBJECTID;
+								$("#" + t.id + "selectCountry").val(obid).trigger("chosen:updated").trigger("change");						
+							}else{
+								t.obj.visibleLayers = [t.countries];
+								t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+								$("#" + t.id + "selectCountry").val("").trigger("chosen:updated").trigger("change");	
+							}	
+						})	
+					}
+				})
+			}				
+		});
+    }
+);
