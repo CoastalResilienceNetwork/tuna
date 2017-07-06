e29c9ef51522245bd349fab70963cc69a164b974
Casey Schneebeck <cschneebeck@tnc.org>
2017-07-06 13:59:50 -0600

    add circles to tabs

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, conte
 	return declare(PluginBase, {
 		// The height and width are set here when an infographic is defined. When the user click Continue it rebuilds the app window with whatever you put in.
 		toolbarName: "Natural Defense Projects", showServiceLayersInLegend: true, allowIdentifyWhenActive: false, rendered: false, resizable: false,
-		hasCustomPrint: false, size:'custom', width:430, hasHelp:false, 
+		hasCustomPrint: false, size:'custom', width:400, hasHelp:false, 
 		
 		// First function called when the user clicks the pluging icon. 
 		initialize: function (frameworkParameters) {
